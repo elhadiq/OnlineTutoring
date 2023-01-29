c80ae31f95c34e724f50cd1e4e0708d19b7c936f
Zouhair El hadiq <zouhairelhadiq@gmail.com>
2023-01-29 14:37:28 +0100

    Todo CopyOf

---
+++ src/exceptions/RectangleException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class RectangleException extends Exception{
+
+}
--- src/geometry/Rectangle.java
+++ src/geometry/Rectangle.java
@@ -1,12 +1,17 @@
 package geometry;
 
+import org.w3c.dom.css.Rect;
+
 import java.util.EnumMap;
 import java.util.List;
 
-public final class Rectangle <T>{
+public final class Rectangle <T extends Comparable>{
+
+
+
     private final EnumMap<Direction, T> borders;
 
-    public Rectangle(EnumMap<Direction, T> borders) {
+    private Rectangle(EnumMap<Direction, T> borders) {
         this.borders = borders;
     }
     T getBorder(Direction direction){
@@ -24,6 +29,22 @@ public final class Rectangle <T>{
     public T bottom(){return getBorder(Direction.BOTTOM);}
     public T top() {return getBorder(Direction.TOP);}
     public T left (){return getBorder(Direction.LEFT);}
-    public T right(){return getBorder(Direction.RIGHT);}
+    public T right(){
+        return getBorder(Direction.RIGHT);}
+
+    public Rectangle<T> of (T top,T bottom,T left,T right) throws Exception{
+        assert bottom.compareTo(top)<0 ||left.compareTo(right)<0:"The borders are out of order";
+        EnumMap<Direction, T> borders=new EnumMap<>(Direction.class);
+        borders.put(Direction.TOP,top);
+        borders.put(Direction.BOTTOM,bottom);
+        borders.put(Direction.LEFT,left);
+        borders.put(Direction.RIGHT,right);
+        return new Rectangle<>(borders);
+    }
+
+    public Rectangle<T> copyOf(Rectangle<T> rect){
+        //TODO
+        return rect;
+    }
 
 }
